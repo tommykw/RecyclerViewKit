5c67ee7542aa6967b0e7e0b2864031c75fb38ec5
kenji tomita <destinys.lotus.1108mn@gmail.com>
2017-07-06 09:09:46 +0900

    Initial commit

---
+++ src/main/kotlin/ResourceFinderAction.kt
@@ -0,0 +1,7 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ResourceFinderAction : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+    }
+}
