4a5cf01bf2d253ecf4c085856163afa93b0bc743
kenji tomita <destinys.lotus.1108mn@gmail.com>
2017-07-07 09:11:45 +0900

    Modify plugin settings

--- src/main/kotlin/ResourceFinderAction.kt
+++ src/main/kotlin/ResourceFinderAction.kt
@@ -1,7 +1,10 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
 
 class ResourceFinderAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project
+        Messages.showMessageDialog(project, "", "", Messages.getInformationIcon())
     }
 }
